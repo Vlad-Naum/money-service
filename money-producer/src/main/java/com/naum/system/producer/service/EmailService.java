3fc7b8f9ef191c18baac589c93a13e45de7a863a
vnaumkin <naumkin.vlad2012@yandex.ru>
2024-05-09 10:23:43 +0300

    feat: добавлено получение user.email из сервиса в producer

---
+++ money-producer/src/main/java/com/naum/system/producer/service/EmailService.java
@@ -0,0 +1,39 @@
+package com.naum.system.producer.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class EmailService {
+
+    private static final Pattern PATTERN = Pattern.compile("\"email\":\"(.*)\"");
+    private static final String DEFAULT_EMAIL = "test@test.com";
+
+    @Value(value = "${email.resource.url}")
+    private String emailResourceUrl;
+
+    public List<String> getEmails() {
+        RestTemplate restTemplate = new RestTemplate();
+        System.out.println(emailResourceUrl);
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(emailResourceUrl, String.class);
+        String body = response.getBody();
+        if (body == null) {
+            return List.of(DEFAULT_EMAIL);
+        }
+        List<String> emails = new ArrayList<>();
+        Matcher matcher = PATTERN.matcher(response.getBody());
+        while (matcher.find()) {
+            String email = matcher.group(1);
+            emails.add(email);
+        }
+        return emails;
+    }
+}
--- money-producer/src/main/java/com/naum/system/producer/service/ScheduledService.java
+++ money-producer/src/main/java/com/naum/system/producer/service/ScheduledService.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -16,13 +17,17 @@ public class ScheduledService {
     @Autowired
     private KafkaProducerService producerService;
 
+    @Autowired
+    private EmailService emailService;
+
     @Scheduled(fixedDelay = 60, timeUnit = TimeUnit.SECONDS, initialDelay = 0)
     public void scheduleKafkaProduced() {
+        List<String> emails = emailService.getEmails();
         Random random = new Random();
         MoneyCostsKafka moneyCostsKafka = new MoneyCostsKafka();
         moneyCostsKafka.setMoneyCostsCategoryId(random.nextInt(0, MoneyCostsCategory.values().length));
         moneyCostsKafka.setExpenses(random.nextLong(100000));
-        moneyCostsKafka.setUserEmail("test@test.com");
+        moneyCostsKafka.setUserEmail(emails.get(random.nextInt(emails.size())));
         moneyCostsKafka.setLocalDateTime(LocalDateTime.now());
         producerService.sendMessage(moneyCostsKafka);
     }
